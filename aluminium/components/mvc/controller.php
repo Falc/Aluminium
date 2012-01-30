b764221e5eb0c87f93bbcc7d5ff42c9fc59e643e
Aitor García <aitor.falc@gmail.com>
2012-01-30 18:58:25 +0100

    Controller and Model skeletons.

---
+++ aluminium/components/mvc/controller.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file contains the Controller class.
+ *
+ * @author		Aitor García <aitor.falc@gmail.com>
+ * @copyright	2012 Aitor García <aitor.falc@gmail.com>
+ * @license		https://github.com/Falc/Aluminium/blob/master/LICENSE New BSD License
+ * @package		Aluminium
+ * @subpackage	Components
+ */
+
+/**
+ * The parent class for controllers.
+ *
+ * @package		Aluminium
+ * @subpackage	Components
+ */
+abstract class Controller {
+
+	public function __construct() {
+	}
+
+}
+
+?>
---
+++ aluminium/components/mvc/model.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file contains the Model class.
+ *
+ * @author		Aitor García <aitor.falc@gmail.com>
+ * @copyright	2012 Aitor García <aitor.falc@gmail.com>
+ * @license		https://github.com/Falc/Aluminium/blob/master/LICENSE New BSD License
+ * @package		Aluminium
+ * @subpackage	Components
+ */
+
+/**
+ * The parent class for models.
+ *
+ * @package		Aluminium
+ * @subpackage	Components
+ */
+abstract class Model {
+
+	public function __construct() {
+	}
+
+}
+
+?>
