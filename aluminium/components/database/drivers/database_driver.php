046f4cd1116b20d916d170ea990f0e62716e8879
Aitor García <aitor.falc@gmail.com>
2012-02-05 20:30:19 +0100

    DatabaseDriver abstract class.

---
+++ aluminium/components/database/drivers/database_driver.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file contains the DatabaseDriver class.
+ *
+ * @author		Aitor García <aitor.falc@gmail.com>
+ * @copyright	2012 Aitor García <aitor.falc@gmail.com>
+ * @license		https://github.com/Falc/Aluminium/blob/master/LICENSE New BSD License
+ * @package		Aluminium
+ * @subpackage	Components
+ */
+
+/**
+ * The parent class for database drivers.
+ *
+ * @package		Aluminium
+ * @subpackage	Components
+ */
+abstract class DatabaseDriver {
+
+	public function __construct() {
+	}
+
+	abstract function connect($conf);
+
+}
+
+?>
