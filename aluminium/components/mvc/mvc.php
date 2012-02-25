2e2060e8d623eccee16a8adff2f92c2fa03ea245
Aitor García <aitor.falc@gmail.com>
2012-02-26 00:31:05 +0100

    Adding a controller loader method to the MVC component.

--- aluminium/components/mvc/mvc.php
+++ aluminium/components/mvc/mvc.php
@@ -38,6 +38,37 @@ class MVC {
 		require_once(ALUMINIUM_COMPONENTS.'mvc/classes/controller.php');
 	}
 
+	/**
+	 * Creates a new controller instance specified by $name and sets its $parameters.
+	 *
+	 * Since Controller is an abstract class, $name should refer to a class that extends it.
+	 *
+	 * @param	string	$name		The name of the class to instance.
+	 * @param	mixed	$parameters	A list of parameters or null if any.
+	 */
+	public function load_controller($class, $parameters) {
+		$class_file = APP_CONTROLLERS.$class.'.php';
+
+		// If the class file does not exist, stop the process
+		if(!file_exists($class_file)) {
+			die('Error: File "'.$class_file.'" not found.');
+		}
+
+		// Include the controller class file
+		require_once($class_file);
+
+		// If the class does not exist, stop the process
+		if(!class_exists($class, FALSE)) {
+			die('Error: Class "'.$class.'" not found.');
+		}
+
+		// Create the controller instance and set the parameters
+		$controller = new $class();
+		$controller->set_parameters($parameters);
+
+		return $controller;
+	}
+
 }
 
 ?>
