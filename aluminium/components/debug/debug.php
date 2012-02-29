cc341e63c116884c4c3b479c9e3c3619a8f976c4
Aitor García <aitor.falc@gmail.com>
2012-02-29 02:55:40 +0100

    Many changes to the Debug component. Now it can process log files.

--- aluminium/components/debug/debug.php
+++ aluminium/components/debug/debug.php
@@ -17,30 +17,21 @@
  */
 class Debug {
 	/**
-	 * List of all the debugging values related to the core.
-	 *
-	 * Format:
-	 * key => value
-	 *
-	 * @var array
-	 */
-	public $core_data;
-
-	/**
-	 * List of all the debugging values related to each component.
-	 *
-	 * Format:
-	 * component1 => array(
+	 * List of all the debugging values stored into a multi-level array:
+	 * core => array(
 	 *     key1 => value1,
 	 *     key2 => value2
 	 * ),
-	 * component2 => array(
+	 * componentN => array(
+	 *     keyA => array(
+	 *         ...
+	 *     ),
 	 *     ...
 	 * )
 	 *
 	 * @var array
 	 */
-	public $components_data;
+	public $data;
 
 	/**
 	 * Tells whether the debug process is running or not.
@@ -55,8 +46,7 @@ class Debug {
 	 * Defines some constants and loads the required configuration.
 	 */
 	public function __construct() {
-		$this->core_data = array();
-		$this->components_data = array();
+		$this->data = array();
 		$this->is_running = FALSE;
 	}
 
@@ -69,10 +59,10 @@ class Debug {
 			return;
 		}
 
-		$this->core_data['memory_usage'] = memory_get_usage();
+		$this->data['core']['memory_usage'] = memory_get_usage();
 
 		$start_time = microtime(true);
-		$this->core_data['start_time'] = $start_time;
+		$this->data['core']['start_time'] = $start_time;
 
 		$this->is_running = TRUE;
 	}
@@ -88,58 +78,140 @@ class Debug {
 
 		// The real_memory_usage info only works for PHP >= 5.2.0
 		if(version_compare(phpversion(), '5.2.0') >= 0) {
-			$this->core_data['real_memory_usage'] = memory_get_usage(true).' bytes.';
+			$this->data['core']['real_memory_usage'] = memory_get_usage(true).' bytes.';
 		}
 
 		$stop_memory_usage = memory_get_usage();
-		$start_memory_usage = $this->core_data['memory_usage'];
+		$start_memory_usage = $this->data['core']['memory_usage'];
 
 		$stop_time = microtime(true);
-		$this->core_data['stop_time'] = $stop_time;
+		$this->data['core']['stop_time'] = $stop_time;
 
 		$memory_usage = $stop_memory_usage - $start_memory_usage;
-		$this->core_data['memory_usage'] = $memory_usage.' bytes.';
+		$this->data['core']['memory_usage'] = $memory_usage.' bytes.';
 
-		$start_time = $this->core_data['start_time'];
+		$start_time = $this->data['core']['start_time'];
 		$execution_time = $stop_time - $start_time;
-		$this->core_data['execution_time'] = $execution_time.' seconds.';
+		$this->data['core']['execution_time'] = $execution_time.' seconds.';
+
+		$this->process_logs();
 
 		$this->is_running = FALSE;
 	}
 
 	/**
-	 * Adds the key name to every value found in $source.
+	 * Processes a single log file.
 	 *
-	 * @param	array	$source	An associative array of debug values.
+	 * This method gets only the relevant log lines and adds them to the $data array.
+	 * A relevant line will match the following format:
+	 * [Key name] Some message text
+	 *
+	 * @param	string	$log_file_name	Name of the log file to process.
 	 */
-	public function get_data_from(array $source) {
-		$output = '';
+	public function process_log($log_file_name) {
+		$component_data = array();
 
-		foreach($source as $key=>$data) {
-			$output .= $key.': '.$data.'<br />';
+		// Get the log file contents
+		$log_file = file(APP_LOGS.$log_file_name, FILE_SKIP_EMPTY_LINES);
+
+		foreach($log_file as $line) {
+			// Split each line into two parts: timestamp and message
+			$log_content = explode(' | ', $line);
+
+			// Split the timestamp into "normal date" and "miliseconds"
+			$log_timestamp = array();
+			$log_timestamp[] = substr($log_content[0], 0, -5); // i.e: Feb 22 22:38:35
+			$log_timestamp[] = substr($log_content[0], -4);    // i.e: 0.323
+
+			// Build the complete timestamp, including miliseconds
+			$timestamp = floatval(strtotime($log_timestamp[0]).$log_timestamp[1]);
+
+			// Process the lines with a timestamp greater than the start time
+			if($timestamp > $this->data['core']['start_time']) {
+				$log_message = array();
+
+				// Process the relevant lines
+				if(preg_match('/\[([^]]*)\] (.*)/', $log_content[1], $log_message)) {
+					// Convert from 'Key name' to 'key_name'
+					$key = strtolower(str_replace(' ', '_', $log_message[1]));
+
+					// Add the content to the $component_data array
+					if(!array_key_exists($key, $component_data)) {
+						$component_data[$key] = $log_message[2];
+					}
+					else {
+						// If a value was already added, store them as an array
+						if(!is_array($component_data[$key])) {
+							$value = $component_data[$key];
+							$component_data[$key] = array($value);
+						}
+
+						$component_data[$key][] = $log_message[2];
+					}
+				}
+			}
 		}
 
-		return $output;
+		// Add the processed $component_data to the $data array
+		if(!empty($component_data)) {
+			$component_name = substr($log_file_name, 0, -4);
+			$this->data[$component_name] = $component_data;
+		}
+	}
+
+	/**
+	 * Processes all the .log files in the logs directory
+	 */
+	public function process_logs() {
+		if($dir_handle = opendir(APP_LOGS)) {
+			// Run process_log() for every .log file
+			while(($file = readdir($dir_handle)) !== FALSE) {
+				if(pathinfo($file, PATHINFO_EXTENSION) === 'log') {
+					$this->process_log($file);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Formats the debugging data for displaying it properly.
+	 *
+	 * This is a recursive method.
+	 *
+	 * @param	string	$name	Name of the current block.
+	 * @param	mixed	$data	An array of elements or a single element.
+	 */
+	public function format_data($name, $data) {
+		$output = '';
+
+		// If $data is a single element, return it
+		if(!is_array($data)) {
+			return $name.': '.$data;
+		}
+		// If $data is an array, run format_data() recursively
+		else {
+			if(!is_null($name)) {
+				$output .= $name.':'."\n";
+			}
+
+			$output .= '<ul>'."\n";
+			foreach($data as $name=>$content) {
+				$output .= '<li>'.$this->format_data($name, $content).'</li>'."\n";
+			}
+			$output .= '</ul>'."\n";
+
+			return $output;
+		}
 	}
 
 	/**
 	 * Displays the debug info.
 	 */
 	public function display() {
-		echo $this->get_data_from($this->core_data).'<br />';
-
-		// If the component data is empty, stop the process
-		if (!empty($this->components_data)) {
-			return;
-		}
-
-		foreach($this->components_data as $component=>$data) {
-			if(!empty($data)) {
-				echo '<strong>'.$component.'</strong><br />';
-				echo $this->get_data_from($data).'<br />';
-			}
-		}
+		$output = $this->format_data(null, $this->data);
+		echo $output;
 	}
+
 }
 
 ?>
