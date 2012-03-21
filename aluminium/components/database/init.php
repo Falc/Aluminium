48fe868ab61677eb723d6ad0c5148716b4a7bcca
Aitor García <aitor.falc@gmail.com>
2012-03-22 00:58:12 +0100

    DB_DRIVERS_PATH is not used.

--- aluminium/components/database/init.php
+++ aluminium/components/database/init.php
@@ -5,8 +5,6 @@ require_once(dirname(__FILE__).'/classes/database.php');
 require_once(dirname(__FILE__).'/classes/drivers/database_driver.php');
 
 // Constant definitions
-if(!defined('DB_DRIVERS_PATH')) {
-	define('DB_DRIVERS_PATH',	dirname(__FILE__).'/classes/drivers/');
-}
+// None
 
 ?>
