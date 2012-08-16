057da3bdd85506b61448fe0f3ae64c92e4e16307
Aitor García <aitor.falc@gmail.com>
2012-08-16 21:48:37 +0200

    Database\instance.php: Use namespaces.

--- aluminium/components/database/instance.php
+++ aluminium/components/database/instance.php
@@ -3,6 +3,9 @@
 // Load init.php
 require(dirname(__FILE__).'/init.php');
 
+// Namespace import
+use Aluminium\Component\Database\Database;
+
 // Return a DatabaseDriver instance
 return new Database();
 
