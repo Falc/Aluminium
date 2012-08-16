dff39a56f03fc5f6bdbdb1024c426d8f6f1b1c04
Aitor García <aitor.falc@gmail.com>
2012-08-16 18:23:39 +0200

    MVC\Model: Add namespace. Some cleaning.

--- aluminium/components/mvc/classes/model.php
+++ aluminium/components/mvc/classes/model.php
@@ -5,21 +5,12 @@
  * @author		Aitor García <aitor.falc@gmail.com>
  * @copyright	2012 Aitor García <aitor.falc@gmail.com>
  * @license		https://github.com/Falc/Aluminium/blob/master/LICENSE Simplified BSD License
- * @package		Aluminium
- * @subpackage	Components
  */
+namespace Aluminium\Component\MVC;
 
 /**
  * The parent class for models.
- *
- * @package		Aluminium
- * @subpackage	Components
  */
 abstract class Model {
-
-	public function __construct() {
-	}
-
 }
-
 ?>
